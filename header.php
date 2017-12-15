e2d1130b54666754108313dc30fcd206843bde13
Hjalte Daniel Hansen <hej@hjaltedaniel.io>
2017-12-15 23:18:30 +0100

    Google Analytics

--- header.php
+++ header.php
@@ -11,6 +11,22 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-15079465-7"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'UA-15079465-7');
+
+    </script>
+
+
+
     <!--Stylesheets-->
     <link href="<?php bloginfo('stylesheet_url');?>" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300|Ubuntu" rel="stylesheet">
